8a3b0c84582b8e1e6a756520ef3be1847da51b83
gface34rus <89259112970@mail.ru>
2024-06-05 11:41:00 +0300

    Homework is done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkYear(LocalDate.now().getYear());
+        getApp(1, 2023);
+        calculateDelivery(100);
+    }
+
+    public static void checkYear(int year) {
+        System.out.println("Задача 1");
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+            System.out.println(year + " год — високосный год");
+        } else {
+            System.out.println(year + " год — невисокосный год");
+        }
+        System.out.println();
+    }
+
+    public static void getApp(int oS, int deviceYear) {
+        System.out.println("Задача 2");
+        if (oS == 0) {
+            if (deviceYear < LocalDate.now().getYear()) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+            }
+        } else if (oS == 1) {
+            if (deviceYear < LocalDate.now().getYear()) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке.");
+            }
+        }
+        System.out.println();
+    }
+
+    public static int calculateDelivery(int deliveryDistance) {
+        System.out.println("Задача 3");
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: 1.");
+            return 1;
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: 2.");
+            return 2;
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            System.out.println("Потребуется дней: 3.");
+            return 3;
+        } else {
+            System.out.println("В ваш район, доставка не производится.");
+            return 0;
+        }
     }
 }
